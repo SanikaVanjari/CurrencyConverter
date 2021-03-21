cfed069186624bae787683beec4dcc81a5dfbffe
Sanika Vanjari <sanikavanjari94@gmail.com>
2021-03-21 20:52:16 +0530

    viewmodel and coroutine setup

--- app/src/main/java/com/practice/currencyconverter/di/AppModule.kt
+++ app/src/main/java/com/practice/currencyconverter/di/AppModule.kt
@@ -1,10 +1,16 @@
 package com.practice.currencyconverter.di
 
 import com.practice.currencyconverter.data.CurrencyApi
+import com.practice.currencyconverter.main.DefaultMainRepository
+import com.practice.currencyconverter.main.MainRepository
+import com.practice.currencyconverter.util.DispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import okhttp3.Dispatcher
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -18,4 +24,22 @@ object AppModule {
         Retrofit.Builder().baseUrl("https://api.exchangeratesapi.io/")
             .addConverterFactory(GsonConverterFactory.create()).build()
             .create(CurrencyApi::class.java)
+
+    @Singleton
+    @Provides
+    fun providesMainRepository(api: CurrencyApi): MainRepository = DefaultMainRepository(api)
+
+    @Singleton
+    @Provides
+    fun providesDispatchers() : DispatcherProvider = object : DispatcherProvider{
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+
+    }
 }
---
+++ app/src/main/java/com/practice/currencyconverter/util/DispatcherProvider.kt
@@ -0,0 +1,11 @@
+package com.practice.currencyconverter.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface DispatcherProvider {
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val unconfined: CoroutineDispatcher
+
+}
