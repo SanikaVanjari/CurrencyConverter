cb73d42f2c816415830240ce280806a2887dfb95
Sanika Vanjari <sanikavanjari94@gmail.com>
2021-03-21 19:22:02 +0530

    viewbinding setup

--- app/src/main/java/com/practice/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/practice/currencyconverter/MainActivity.kt
@@ -1,11 +1,14 @@
-bupackage com.practice.currencyconverter
+package com.practice.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.practice.currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
