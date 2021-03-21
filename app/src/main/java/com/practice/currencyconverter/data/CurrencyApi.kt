adf1a65d6c97f29bc95edbeaf3f4bcc3f0edbac1
Sanika Vanjari <sanikavanjari94@gmail.com>
2021-03-21 19:41:49 +0530

    module class added

---
+++ app/src/main/java/com/practice/currencyconverter/data/CurrencyApi.kt
@@ -0,0 +1,11 @@
+package com.practice.currencyconverter.data
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CurrencyApi {
+
+    @GET("/latest")
+    suspend fun getRates(@Query("base") base: String): Response<CurrencyApi>
+}
